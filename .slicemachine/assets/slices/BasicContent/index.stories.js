e58d889efa8672b97abd8607e1005f4275e46428
John Rae <john.rae23@gmail.com>
2023-01-04 20:08:05 -0500

    Initial Commit

---
+++ .slicemachine/assets/slices/BasicContent/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/BasicContent';
+
+export default {
+  title: 'slices/BasicContent'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"body":[{"type":"paragraph","text":"Adipisicing est officia fugiat.","spans":[]}]},"slice_type":"basic_content","id":"_Default"}} />
+_Default.storyName = ''

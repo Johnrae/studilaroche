a6a1afdc4e1c5e97c2e54d14987c1d5ad912ed9a
John Rae <john.rae23@gmail.com>
2023-02-03 01:53:02 -0500

    Fix revalidate, update header links, and add work links

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 
 export default function Header() {
+  const linkClasses = 'hover:underline underline-offset-4 py-1.5 px-2.5'
   return (
     <div className='w-full bg-white px-10 py-5'>
       <div className='w-full'>
@@ -12,10 +13,18 @@ export default function Header() {
 
       <div className='w-full grid grid-cols-4 gap-10 mt-4'>
         <div className='col-span-4 md:col-start-3 md:col-span-2 flex w-full justify-between'>
-          <Link href='/about'>About</Link>
-          <Link href='/services'>Services</Link>
-          <Link href='/work'>Work</Link>
-          <a href='mailto:ben@studilaroche.com'>Contact</a>
+          <Link className={linkClasses} href='/about'>
+            About
+          </Link>
+          <Link className={linkClasses} href='/services'>
+            Services
+          </Link>
+          <Link className={linkClasses} href='/work'>
+            Work
+          </Link>
+          <a className={linkClasses} href='mailto:ben@studilaroche.com'>
+            Contact
+          </a>
         </div>
       </div>
     </div>
--- pages/work/index.tsx
+++ pages/work/index.tsx
@@ -12,22 +12,24 @@ export default function WorkPage({ data }: { data: any[] | null }) {
       <div className='grid grid-cols-2 md:grid-cols-4 gap-10 px-10'>
         {data?.map((work) => (
           <div key={work.id} className='group hover:cursor-pointer'>
-            <div className='relative  '>
-              <Image
-                className='grayscale group-hover:grayscale-0 transition-all'
-                src={work.data.album_art.square.url}
-                alt={`album art for the album ${work.data.title} by ${work.data.artist}`}
-                width={500}
-                height={500}
-              />
-              <div className='bg-cyan-600 h-full w-full mix-blend-screen group-hover:opacity-0 transition-all absolute inset-0'></div>
-            </div>
-            <p>{work.data.title}</p>
-            <p>{work.data.artist}</p>
+            <a href={work.data.url} target='_blank' rel='noreferrer'>
+              <div className='relative'>
+                <Image
+                  className='grayscale group-hover:grayscale-0 transition-all'
+                  src={work.data.album_art.square.url}
+                  alt={`album art for the album ${work.data.title} by ${work.data.artist}`}
+                  width={500}
+                  height={500}
+                />
+                <div className='bg-cyan-600 h-full w-full mix-blend-screen group-hover:opacity-0 transition-all absolute inset-0'></div>
+              </div>
+              <p>{work.data.title}</p>
+              <p>{work.data.artist}</p>
 
-            <div className='prose text-2xl col-span-3'>
-              <PrismicRichText field={work.data.description} />
-            </div>
+              <div className='prose text-2xl col-span-3'>
+                <PrismicRichText field={work.data.description} />
+              </div>
+            </a>
           </div>
         ))}
       </div>
